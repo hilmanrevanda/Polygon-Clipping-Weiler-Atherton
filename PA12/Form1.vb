57002e1e67e6c149d46894ec5e2712d7505259cc
Teuku Hilman Revanda <thilmanrevanda@gmail.com>
2017-10-15 22:13:00 +0700

    Menambahkan fungsi button SPolygon & MPolygon dengan menggunakan Variable ButtonMenu

--- PA12/Form1.vb
+++ PA12/Form1.vb
@@ -1,10 +1,13 @@
 ﻿Imports System.Drawing.Drawing2D
 
 Public Class MainWindow
+    'To know which button chosen.
+    Private ButtonMenu As String
     ' Each polygon is represented by a List(Of Point).
     Private Polygons As New List(Of List(Of Point))()
 
     ' Points for the new polygon.
+    ' Ambil nilai NewPolygon pada line 24 sebelum di hapus untuk record coordinate setiap poligon guna pengaplikasian ke rumus nantinya, atau ada cara lain?
     Private NewPolygon As List(Of Point) = Nothing
 
     ' The current mouse position while drawing a new polygon.
@@ -13,36 +16,44 @@ Public Class MainWindow
     ' Start or continue drawing a new polygon.
 
     Private Sub picCanvas_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles picCanvas.MouseDown
-        ' See if we are already drawing a polygon.
-        If (NewPolygon IsNot Nothing) Then
-            ' We are already drawing a polygon.
-            ' If it's the right mouse button, finish this polygon.
-            If (e.Button = MouseButtons.Right) Then
-                ' Finish this polygon.
-                If (NewPolygon.Count > 2) Then Polygons.Add(NewPolygon)
-                NewPolygon = Nothing
-            Else
-                ' Add a point to this polygon.
-                If (NewPolygon(NewPolygon.Count - 1) <> e.Location) Then
-                    NewPolygon.Add(e.Location)
+        If ButtonMenu = "SPolygon" Or ButtonMenu = "MPolygon" Then
+            ' See if we are already drawing a polygon.
+            If (NewPolygon IsNot Nothing) Then
+                ' We are already drawing a polygon.
+                ' If it's the right mouse button, finish this polygon.
+                If (e.Button = MouseButtons.Right) Then
+                    ' Finish this polygon.
+                    If (NewPolygon.Count > 2) Then Polygons.Add(NewPolygon) 'NewPolygon store coordinate
+                    'Remove current polygon coordinate
+                    NewPolygon = Nothing
+                    If ButtonMenu = "SPolygon" Then
+                        ButtonMenu = Nothing
+                    End If
+                Else
+                    ' Add a point to this polygon.
+                    If (NewPolygon(NewPolygon.Count - 1) <> e.Location) Then
+                        NewPolygon.Add(e.Location)
+                    End If
                 End If
+            Else
+                ' Start a new polygon.
+                NewPolygon = New List(Of Point)()
+                NewPoint = e.Location
+                NewPolygon.Add(e.Location)
             End If
-        Else
-            ' Start a new polygon.
-            NewPolygon = New List(Of Point)()
-            NewPoint = e.Location
-            NewPolygon.Add(e.Location)
-        End If
 
-        ' Redraw.
-        picCanvas.Invalidate()
+            ' Redraw.
+            picCanvas.Invalidate()
+        End If
     End Sub
 
     ' Move the next point in the new polygon.
     Private Sub picCanvas_MouseMove(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles picCanvas.MouseMove
-        If (NewPolygon Is Nothing) Then Exit Sub
-        NewPoint = e.Location
-        picCanvas.Invalidate()
+        If ButtonMenu = "SPolygon" Or ButtonMenu = "MPolygon" Then
+            If (NewPolygon Is Nothing) Then Exit Sub
+            NewPoint = e.Location
+            picCanvas.Invalidate()
+        End If
     End Sub
 
     ' Redraw old polygons in blue. Draw the new polygon in green.
@@ -53,7 +64,7 @@ Public Class MainWindow
 
         ' Draw the old polygons.
         For Each polygon As List(Of Point) In Polygons
-            e.Graphics.FillPolygon(Brushes.White, polygon.ToArray())
+            e.Graphics.FillPolygon(Brushes.Transparent, polygon.ToArray())
             e.Graphics.DrawPolygon(Pens.Blue, polygon.ToArray())
         Next polygon
 
@@ -84,12 +95,13 @@ Public Class MainWindow
     Private Sub picCanvas_Click(sender As Object, e As EventArgs) Handles picCanvas.Click
 
     End Sub
-    Private Sub btnDrawSPolygon_Click(sender As Object, e As EventArgs) Handles btnDrawSPolygon.Click
 
+    Private Sub btnDrawSPolygon_Click(sender As Object, e As EventArgs) Handles btnDrawSPolygon.Click
+        ButtonMenu = "SPolygon"
     End Sub
 
     Private Sub btnDrawMPolygon_Click(sender As Object, e As EventArgs) Handles btnDrawMPolygon.Click
-
+        ButtonMenu = "MPolygon"
     End Sub
 
     Private Sub btnClipRectangular_Click(sender As Object, e As EventArgs) Handles btnClipRectangular.Click
@@ -123,6 +135,4 @@ Public Class MainWindow
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
         End
     End Sub
-
-
 End Class
